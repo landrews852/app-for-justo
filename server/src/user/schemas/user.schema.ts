37b699b579b5eb48f7216cb69f308cf6b5e9ef5f
Lucas Andrews <andrews852@gmail.com>
2022-08-12 16:34:35 -0400

    update

---
+++ server/src/user/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Field } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  //   @Field
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  breed: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(User);
